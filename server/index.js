a3f0940dc7a156b079c07dbe54261b3fd643e082
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-02-14 19:37:27 +0530

    Backend Setup complete

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import connectDb from "./db/connect.js";
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World!' })
+})
+
+const startServer = async () => {
+    try {
+        connectDb(process.env.MONGODB_URL)
+        app.listen(8000, () => {
+            console.log(`Server started on port http://localhost/8000`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
